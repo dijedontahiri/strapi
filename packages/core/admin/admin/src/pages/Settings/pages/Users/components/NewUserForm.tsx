3ec7fefae253fde3f79584b0d14091b0007b0902
Dijedon <99071957+dijedontahiri@users.noreply.github.com>
2026-09-17 22:21:35 +0200

    fix(admin): block all outside invite modal interactions

--- packages/core/admin/admin/src/pages/Settings/pages/Users/components/NewUserForm.tsx
+++ packages/core/admin/admin/src/pages/Settings/pages/Users/components/NewUserForm.tsx
@@ -138,7 +138,7 @@ const ModalForm = ({ onToggle }: ModalFormProps) => {
 
   return (
     <Modal.Root defaultOpen onOpenChange={onToggle}>
-      <Modal.Content onPointerDownOutside={(event) => event.preventDefault()}>
+      <Modal.Content onInteractOutside={(event) => event.preventDefault()}>
         <Modal.Header>
           {/**
            * TODO: this is not semantically correct and should be amended.
